804eb6d59e3850ab307fd30e05df8ff6a88120a1
chelcx2 <100275141+chelcx2@users.noreply.github.com>
2022-08-03 11:13:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-import zmq, time, pickle, sys
+import zmq, time, pickle, sys, socket
+
+'''primary socket API functions and methods:
+socket()
+.bind()
+.listen()
+.accept()
+.connect()
+.connect_ex()
+.send()
+.recv()
+.close()
+'''
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+''' creating socket object and then setting up a listening socket for the SERVER side'''
+socket.socket() 
+socket.client()
+'''specifying socket type Note: this protocol default is TCP'''
+socket.SOCK_STREAM
+socket.bind()
+'''listening for connections from CLIENTS'''
+socket.listen()
+''' here it accepts the connection'''
+socket.accept()
+
 
 context = zmq.Context()
 me = str(sys.argv[1])
@@ -12,6 +39,8 @@ while True: # generate 100 workloads
     pass
     #workload = random.randint(1, 100) # compute workload
     #s.send(pickle.dumps((me,workload))) # send workload to worker
+
+
 
 
 #context = zmq.Context()
